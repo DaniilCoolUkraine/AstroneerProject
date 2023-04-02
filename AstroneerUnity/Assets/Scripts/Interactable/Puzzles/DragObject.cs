4d2296760f8776a1bec1140fca60516d0efd04db
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-04-02 19:58:44 +0300

    added labirint handler

---
+++ AstroneerUnity/Assets/Scripts/Interactable/Puzzles/DragObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Astroneer.Interactable.Puzzles
+{
+    public class DragObject : MonoBehaviour, IDragHandler, IEndDragHandler
+    {
+        [SerializeField] private Rigidbody2D _rb;
+        
+        private Vector3 _mouseOffset;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            Vector3 mouseWorldPos = _camera.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0f;
+            
+            if (eventData.delta.magnitude == 0)
+            {
+                _mouseOffset = transform.position - mouseWorldPos;
+            }
+            
+            _rb.MovePosition(mouseWorldPos + _mouseOffset);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _rb.velocity = Vector2.zero;
+        }
+    }
+}
